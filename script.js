f0be3d1828c877aef57ac258c8d5e711e8c64403
Bruno <bdorigon@hotmail.com>
2023-07-11 09:40:17 -0300

    add-funcao-geral

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const horas = document.querySelector(".hours");
-const minutos = document.querySelector(".minutes");
-const segundos = document.querySelector(".seconds");
+function clock() {
+  const horas = document.querySelector(".hours");
+  const minutos = document.querySelector(".minutes");
+  const segundos = document.querySelector(".seconds");
 
-function setClock() {
-  const data = new Date();
-  const hours = data.getHours().toString().padStart(2, "0");
-  const minutes = data.getMinutes().toString().padStart(2, "0");
-  const seconds = data.getSeconds().toString().padStart(2, "0");
+  function setClock() {
+    const data = new Date();
+    const hours = data.getHours().toString().padStart(2, "0");
+    const minutes = data.getMinutes().toString().padStart(2, "0");
+    const seconds = data.getSeconds().toString().padStart(2, "0");
 
-  horas.textContent = `${hours} :`;
-  minutos.textContent = `${minutes} :`;
-  segundos.textContent = `${seconds}`;
+    horas.textContent = `${hours} :`;
+    minutos.textContent = `${minutes} :`;
+    segundos.textContent = `${seconds}`;
+  }
+
+  function updateClock() {
+    setInterval(setClock, 1000);
+  }
+  updateClock();
 }
-
-function updateClock() {
-  setInterval(setClock, 1000);
-}
-
-window.addEventListener("load", updateClock);
+window.addEventListener("load", clock);
